40441825eb3d85f7205983dc5795569accc3121f
Cooper Gradishar <coopergradishar@Coopers-MacBook-Pro.local>
2026-05-06 14:29:47 -0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,14 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         command = command.trim().to_string();
-        if command == "exit" {
+        if command.starts_with("echo ") {
+            println!("{}", &command[5..]);
+        }
+        else if command == "exit" {
             break;
         }
-        println!("{}: command not found", command.trim())
+        else{
+            println!("{}: command not found", command.trim())
+        }
     }
 }
